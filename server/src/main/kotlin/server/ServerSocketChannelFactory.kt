f11cf1114b09282e90a323169e6fcd792b4465b0
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2024-03-30 19:50:39 +0100

    Add Socket factory contract and implementation

---
+++ server/src/main/kotlin/server/ServerSocketChannelFactory.kt
@@ -0,0 +1,32 @@
+package io.github.akmal2409.nats.server.server
+
+import java.net.InetSocketAddress
+import java.net.ProtocolFamily
+import java.net.StandardProtocolFamily
+import java.nio.channels.Selector
+import java.nio.channels.ServerSocketChannel
+
+fun interface SelectorFactory {
+
+    fun create(): Selector
+}
+
+class SelectorFactoryImpl : SelectorFactory {
+    override fun create() = Selector.open()
+}
+
+fun interface ServerSocketChannelFactory {
+
+    fun create(): ServerSocketChannel
+}
+
+class NioTcpServerSocketChannelFactory(
+    val bindAddress: InetSocketAddress = InetSocketAddress.createUnresolved("localhost", 0),
+    val protocolFamily: ProtocolFamily = StandardProtocolFamily.INET
+): ServerSocketChannelFactory {
+
+    override fun create(): ServerSocketChannel = ServerSocketChannel.open(protocolFamily).apply {
+        bind(bindAddress)
+        configureBlocking(false)
+    }
+}
