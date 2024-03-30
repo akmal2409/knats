b2912f34ed0b9d443e873f6ad1a72c9b3ec86d99
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2024-03-30 19:49:12 +0100

    Add Kotlin logging and logback dependency

--- gradle-plugins/src/main/kotlin/knats.kotlin-conventions.gradle.kts
+++ gradle-plugins/src/main/kotlin/knats.kotlin-conventions.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 }
 
 dependencies {
+    implementation("ch.qos.logback:logback-classic:1.5.3")
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
